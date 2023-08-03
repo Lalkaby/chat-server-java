56c3b80650c34f39c8923e86b216ee913e7e21ba
Yan Temnyakov <temnyakovyan@gmail.com>
2023-08-03 16:20:29 +0300

    ready to push'

--- src/main/java/by/temniakov/api/controller/LoginController.java
+++ src/main/java/by/temniakov/api/controller/LoginController.java
@@ -3,10 +3,12 @@ package by.temniakov.api.controller;
 import by.temniakov.api.dto.AckDTO;
 import by.temniakov.store.entity.UserEntity;
 import by.temniakov.store.repository.UserRepository;
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.AllArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @RestController
@@ -21,9 +23,13 @@ public class LoginController {
     public static final String LOGOUT_USER = "/logout";
 
     @PostMapping(LOGIN_USER)
-    public UUID loginNewUser(
-            @RequestParam(name = "username",required = true) String username ){
-         userRepository.findByUsername(username)
+    public UUID loginNewUser(@RequestBody(required = false) JsonNode request,
+            @RequestParam(name = "username") String username ){
+
+        //JsonNode nodeUsername = request.get("username");
+
+      //  username = nodeUsername.asText();
+        userRepository.findByUsername(username)
                  .ifPresent(s -> {throw new RuntimeException("Already in chat");});
 
        UserEntity user = UserEntity.builder().username(username).build();
